2c9a105f860442efd761d3024c9916f2b6a022ba
Tristen <tristen.hammon21@gmail.com>
2022-09-06 10:21:37 -0500

    commit tree 43d0d8a582f89aa069e1108ab11b27b0e46494f1

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
+
+
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => renderBooks(data));
+
 }
 
 function renderBooks(books) {
+  console.log("Hi being called.")
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    h2.innerHTML = book.name
+    main.appendChild(h2)
+  })
 }
+
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+//   fetch('https://anapioficeandfire.com/api/books')
+//   .then(response => response.json())
+//   .then(data => console.log(data));  
+// }
+  
+  
+//   fetch('https://anapioficeandfire.com/api/books')
+//   .then(response => response.json())
+//   .then(data => console.log(data));  
+// }
+
+// function fetchBooks() {
+//   fetch('https://anapioficeandfire.com/api/books')
+//   .then(function(resp){
+//     console.log(resp);
+//     return resp.json();
+//   }) 
+//   .then(function(data){
+//     console.log(data);
+//   })
+// }
